d76dc0672bdaaaf15e536c9514d5c07963010140
WangZihao <2597160811@qq.com>
2022-06-28 23:37:54 +0800

    feat(permission.js): 页面鉴权功能完成

--- src/permission.js
+++ src/permission.js
@@ -1,9 +1,23 @@
 import router from './router'
 import store from './store'
 
+// 白名单：未登录状态下可以进入的页面
+const whiteList = ['/login']
+
 router.beforeEach((to, from, next) => {
   /// 获取token
   const token = store.getters.token
-  console.log(token)
-  next()
+  if (token) {
+    if (to.path === '/login') {
+      next(from.path)
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
 })
